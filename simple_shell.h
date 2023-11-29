2b6d17790abcd77a6bb1aa30511b0bc32a23649a
MKDLAMINI <dlaminikhehla5@gmail.com>
2023-11-29 01:31:26 -0800

    created header file with neccessary libraries, added one function that can be used to start up the shel, just need to work on the logic

---
+++ simple_shell.h
@@ -0,0 +1,12 @@
+#include <readline/history.h>
+#include <readline/history.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+void start_new_shell();
+
+#endif
