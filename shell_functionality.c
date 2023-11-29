dd95bc2f4cc7a3a155a9f652cce1ce296d5e8d83
MKDLAMINI <dlaminikhehla5@gmail.com>
2023-11-29 02:11:35 -0800

    created a function that displays greeting for the shell as well as displays user name

---
+++ shell_functionality.c
@@ -0,0 +1,13 @@
+#include "simple_shell.h"
+/**
+ * start_new_shell - function prints out a greeting and a username
+ */
+
+void start_new_shell(void)
+{
+	char *profile_name = getenv("username");
+
+	printf("hello, you are logged in\n");
+
+	printf("profile_name: @%s\n", profile_name);
+}
