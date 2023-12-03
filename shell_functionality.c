1a243ba76a1d81d458c66fa467b6645ba73e49b0
MKDLAMINI <dlaminikhehla5@gmail.com>
2023-12-03 10:25:44 -0800

    updated variables on shell_functionality exe_command and shell_start

--- shell_functionality.c
+++ shell_functionality.c
@@ -3,80 +3,75 @@
  * start_new_shell - function prints out a greeting and a username
  * Description: getenv collects the value of "username"
  */
-
 void start_new_shell(void)
 {
-	char *profile_name = getenv("USER");
+	char *user_avatar = getenv("USER");
 
 	shell_print("hello, you are logged in\n");
 
-	if (profile_name != NULL)
+	if (user_avatar != NULL)
 	{
-
-		shell_print("profile_name: @%s\n");
-
+		shell_print("user_avatar: @%s\n");
 	}
 	else
 	{
-
-		shell_print("Not able to retrieve profile name.\n");
-
+		shell_print("Not able to retrieve user_avatar.\n");
 	}
 }
-
 /**
  * exe_command - function handles system commands
- * @action: system command to be executed
+ * @command: input string that is implemented by the system
+ * Description: this function analysis the input from user and
+ * checks if basic linux system commands are being executed
  */
-
 void exe_command(char *command)
 {
-	char *token_pos, *arguments[MAX_SIZE];
-	int argument_count = 0;
-	pid_t new_process;
+	char *token_stream, *argument_param[MAX_SIZE];
+	int argument_tally = 0;
+	pid_t new_process_id;
 
-	token_pos = strtok(command, " ");
-	while (token_pos != NULL)
+	token_stream = strtok(command, " ");
+
+	while (token_stream != NULL)
 	{
-		arguments[argument_count] = token_pos;
-		argument_count++;
-		token_pos = strtok(NULL, " ");
+		argument_param[argument_tally] = token_stream;
+		argument_tally++;
+		token_stream = strtok(NULL, " ");
 	}
-	arguments[argument_count] = NULL;
-	if (argument_count == 0)
+	argument_param[argument_tally] = NULL;
+	if (argument_tally == 0)
 	{
 		return;
 	}
-	if (strcmp(arguments[0], "exit") == 0) 
+	if (strcmp(argument_param[0], "exit") == 0)
 	{
 		exit_shell();
 	}
-	if (strcmp(arguments[0], "cd") == 0)
+	if (strcmp(argument_param[0], "cd") == 0)
 	{
-		if (change_dir(arguments) == -1)
+		if (change_dir(argument_param) == -1)
 		{
 			return;
 		}
 	}
-	if (strcmp(arguments[0], "ls") == 0)
+	if (strcmp(argument_param[0], "ls") == 0)
 	{
-		list_dir(arguments);
+		list_dir(argument_param);
 	}
-	new_process = fork();
-
-	if (new_process == -1)
+	new_process_id = fork();
+	if (new_process_id == -1)
 	{
 		perror("fork");
 		exit(EXIT_FAILURE);
-	} else if (new_process == 0)
+	} else if (new_process_id == 0)
 	{
-		if (execve(arguments[0], arguments, environ) == -1)
+		if (execve(argument_param[0], argument_param, environ) == -1)
 		{
 			perror("execve");
 			exit(EXIT_FAILURE);
 		}
 	} else
 	{
-		waitpid(new_process, NULL, 0);
+		waitpid(new_process_id, NULL, 0);
 	}
 }
